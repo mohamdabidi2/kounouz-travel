f17582986193e7e6e2e32414d941e4d13fd50caa
mohamdabidi2 <mohamedabidi58@gmail.com>
2022-10-04 13:18:25 +0200

    Configure React app for deployment to GitHub Pages

---
+++ progr.cmd
@@ -0,0 +1,3 @@
+@Echo Off
+cd gestion_de_agence 
+npm start
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import 'animate.css';
+import Login from './components/connection';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './components/home';
+import Caisse from './components/caisse/caisse';
+import SheetGoogleApi from './utils/Services/googleSheetApi';
+import Caiss from './components/caisse/c';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route exact path="/" element={<Login />}/>
+ 
+      <Route exact path="/dashboard" element={<Home />}/>
+      <Route exact path="/react-gh-pages" element={<Login />}/>
+      <Route exact path="/react-gh-pages/dashboard" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
